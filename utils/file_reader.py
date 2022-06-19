247c1677675e5d319e5ce67c4e482a9f6b76de85
ghanteyyy <ghanteyyy@gmail.com>
2022-06-19 11:18:46 +0545

    Refactored code

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -2,10 +2,8 @@ def file_reader(file_name):
     """
     Reads a file and returns a list of each lines as individual element.
     """
-    # loop through each line separately
+
     with open(file_name, 'r') as f:
-        file = f.readlines()
-        # create a list and append each line of file to it after splitting it by comma
-        a = [line.split(',') for line in file]
-        return a
+        # create a list of each line of file after splitting it by comma
+        return [line.split(',') for line in f.readlines()]
 
--- utils/input_parser.py
+++ utils/input_parser.py
@@ -1,6 +1,6 @@
 
 def input_parser(file):
-    'Takes the dict from of each lines, breaks them into another list of values seaprated by comma'
+    'Takes the dict from of each lines, breaks them into another list of values separated by comma'
 
     return [item for item in file]
 
